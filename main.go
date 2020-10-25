ed9a77732556bdb81b7a724f9c8555cd05aa16d6
danilomacb <danilo.bakun@gmail.com>
2020-10-25 09:33:25 -0300

    change home to list, format and handling with cors in the right way

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"context"
-	"io/ioutil"
-	"encoding/json"
 
 	"github.com/gorilla/mux"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var collection *mongo.Collection
 var ctx = context.TODO()
 
 func init() {
-    clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
-    client, err := mongo.Connect(ctx, clientOptions)
-    if err != nil {
-        log.Fatal(err)
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
 	}
-	
+
 	err = client.Ping(ctx, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -40,19 +40,28 @@ func main() {
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", add).Methods("POST")
-	myRouter.HandleFunc("/", home)
+	myRouter.HandleFunc("/", list)
 	myRouter.Use(mux.CORSMethodMiddleware(myRouter))
 	log.Fatal(http.ListenAndServe(":3001", myRouter))
 }
 
 type Element struct {
-	ID primitive.ObjectID `bson:"_id" json:"_id"`
-	Content string `bson:"content" json:"content"`
+	ID      primitive.ObjectID `bson:"_id" json:"_id"`
+	Content string             `bson:"content" json:"content"`
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	
+func setupResponse(w *http.ResponseWriter, r *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+func list(w http.ResponseWriter, r *http.Request) {
+	setupResponse(&w, r)
+	if (*r).Method == "OPTIONS" {
+		return
+	}
+
 	var elements []Element
 
 	cur, err := collection.Find(ctx, bson.D{{}})
@@ -84,11 +93,16 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func add(w http.ResponseWriter, r *http.Request) {
+	setupResponse(&w, r)
+	if (*r).Method == "OPTIONS" {
+		return
+	}
+
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	var element Element
 	element.ID = primitive.NewObjectID()
 	json.Unmarshal(reqBody, &element)
-	
+
 	_, err := collection.InsertOne(ctx, element)
 	if err != nil {
 		log.Fatal(err)
