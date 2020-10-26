27bf322f830e923bf43cc91e48417ca9647d1e80
danilomacb <danilo.bakun@gmail.com>
2020-10-26 09:05:18 -0300

    new connection

--- main.go
+++ main.go
@@ -7,33 +7,42 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var collection *mongo.Collection
 var ctx = context.TODO()
 
-func init() {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
-	client, err := mongo.Connect(ctx, clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			log.Println("Disconnect")
+			panic(err)
+		}
+	}()
 
-	err = client.Ping(ctx, nil)
+	ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	err = client.Ping(ctx, readpref.Primary())
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Error on Ping")
+		panic(err)
 	}
 
 	collection = client.Database("simpleCrudGoMongo").Collection("elements")
-}
 
-func main() {
+	fmt.Println("Server running on port 3001")
+
 	handleRequests()
 }
 
