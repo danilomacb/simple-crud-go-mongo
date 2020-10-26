4390b5c800bcbebad08a50ad5b0c3adef0b44d95
danilomacb <danilo.bakun@gmail.com>
2020-10-26 10:36:17 -0300

    better errors handlers on list

--- main.go
+++ main.go
@@ -77,7 +77,8 @@ func list(w http.ResponseWriter, r *http.Request) {
 
 	cur, err := collection.Find(ctx, bson.D{{}})
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Error on list, fail to find elements:", err)
+		w.WriteHeader(http.StatusInternalServerError)
 	}
 	defer cur.Close(ctx)
 
@@ -85,19 +86,22 @@ func list(w http.ResponseWriter, r *http.Request) {
 		var e Element
 		err := cur.Decode(&e)
 		if err != nil {
-			log.Fatal(err)
+			log.Println("Error on list, fail to decode element:", err)
+			w.WriteHeader(http.StatusInternalServerError)
 		}
 
 		elements = append(elements, e)
 	}
 
 	if err := cur.Err(); err != nil {
-		log.Fatal(err)
+		log.Println("Error on list, something went wrong on findeds elements:", err)
+		w.WriteHeader(http.StatusInternalServerError)
 	}
 
 	elementsJSON, err := json.Marshal(elements)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Error on list, fail to convert to json:", err)
+		w.WriteHeader(http.StatusInternalServerError)
 	}
 
 	fmt.Fprintln(w, string(elementsJSON))
